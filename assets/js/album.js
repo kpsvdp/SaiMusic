5b76dd9e3131e4786c72842d1ecaa018f6754ad1
Pavan Sai Venkata Durga Prashanth Korapati <134833398+kpsvdp@users.noreply.github.com>
2024-10-04 16:09:01 +0100

    Update album.js

--- assets/js/album.js
+++ assets/js/album.js
@@ -6,7 +6,7 @@ const albums = [
         id: 1,
         title: "Guntur Kaaram",
         artist: "Artist 1",
-        cover: "/Users/kpsvdprashanth/Downloads/HTML & CSS/Music Player/assets/img/Guntur Kaarma.jpg", // Add cover image
+        cover: "assets/img/Guntur Kaarma.jpg", // Add cover image
         songs: [
             { title: "Amma", src: "/Users/kpsvdprashanth/Downloads/HTML & CSS/Music Player/assets/Music/Guntur Karam/Amma.mp3" },
             { title: "Dum Masala", src: "/Users/kpsvdprashanth/Downloads/HTML & CSS/Music Player/assets/Music/Guntur Karam/Dum Masala.mp3" },
